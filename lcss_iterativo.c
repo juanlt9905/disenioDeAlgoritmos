9b9bfba22a538f34e7aa9a52827f5b58a4f53d06
juanlt9905 <juanlt9905@gmail.com>
2025-05-06 15:12:07 -0700

    lcss_iterativo

---
+++ lcss_iterativo.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h> 
+#include <string.h>
+#define MAXIMO 1000
+char a[MAXIMO];
+char b[MAXIMO];
+int matriz[MAXIMO][MAXIMO];
+
+int max(int a, int b) { return a > b ? a : b; }
+int main(int argc, char **argv){
+
+
+    //Cadenas de entrada
+    char *a = argv[1];
+    char *b = argv[2];
+
+    int lenA =strlen(a);
+    int lenB=strlen(b);
+
+    
+    for (int i = 0; i<=lenA; i++){
+        for (int j= 0; j<=lenB; j++){
+            matriz[i][j] =0 ;
+        }
+    }
+
+    for (int i = 0; a[i]!='\0'; i++){
+        for (int j= 0; b[j]!='\0'; j++){
+            if(a[i]==b[j])
+                matriz[i+1][j+1]= 1 + matriz[i][j]; 
+            else
+                matriz[i+1][j+1]=max(matriz[i][j+1], matriz[i+1][j]);
+        }
+
+    }
+
+    int resultado = matriz[lenA][lenB];
+    printf("Resultado: %d\n", resultado);
+
+
+
+
+
+
+    return 0;
+}
